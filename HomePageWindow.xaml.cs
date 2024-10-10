6cfa6d3120d3c2e0ebddffb04a4b5d276074152c
anugrahkrishnan <64000608+anugrahkrishnan@users.noreply.github.com>
2024-10-10 12:25:34 +0530

    Add files via upload
    PHTProject added

---
+++ HomePageWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PHTProject
+{
+    /// <summary>
+    /// Interaction logic for HomePageWindow.xaml
+    /// </summary>
+    public partial class HomePageWindow : Window
+    {
+        public HomePageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            PHTConfig.loginWindow.Show();
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            PHTConfig.signUpWindow.Show();
+        }
+
+        private void btnContactSupport_Click(object sender, RoutedEventArgs e)
+        {
+            PHTConfig.contactSupportWindow.Show();
+        }
+    }
+}
---
+++ PHTConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHTProject
+{
+    public static class PHTConfig
+    {
+        public static LoginWindow loginWindow { get; set; }
+        public static SignUpWindow signUpWindow { get; set; }
+        public static HomePageWindow homePageWindow { get; set; }
+        public  static DailyHealthMetrixEntryWindow dailyHealthMetrixEntryWindow { get; set; }
+        public  static DeviceIntgrationWindow deviceIntgrationWindow { get; set; }
+        public  static HealthGoalsWindow healthGoalsWindow { get; set; }
+        public  static HealthHistoryWindow healthHistoryWindow { get; set; }
+        public static HealthInsightWindow healthInsightWindow { get; set; }
+        public  static ProfileSettingsWindow profileSettingsWindow { get; set; }
+        public static UserDashBoardWindow userDashBoardWindow { get; set; }
+        public static ContactSupportWindow contactSupportWindow { get; set; }
+
+        static PHTConfig() 
+        {
+            loginWindow = new LoginWindow();
+            signUpWindow = new SignUpWindow();
+            homePageWindow = new HomePageWindow();
+            dailyHealthMetrixEntryWindow = new DailyHealthMetrixEntryWindow();
+            deviceIntgrationWindow = new DeviceIntgrationWindow();
+            healthGoalsWindow = new HealthGoalsWindow();
+            healthHistoryWindow = new HealthHistoryWindow();
+            healthInsightWindow = new HealthInsightWindow();
+            profileSettingsWindow = new ProfileSettingsWindow();
+            userDashBoardWindow = new UserDashBoardWindow();
+            contactSupportWindow = new ContactSupportWindow();
+        }
+    }
+}
